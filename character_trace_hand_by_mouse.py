081a668f7c7f56459559aef48be811590a9c8486
Hyno03 <jenny2117@naver.com>
2023-09-29 21:08:10 +0900

    Modify handprint code
    use list to remain handprint images

--- character_trace_hand_by_mouse.py
+++ character_trace_hand_by_mouse.py
@@ -10,7 +10,7 @@ hand = load_image('hand_arrow.png')
 def handle_events():
     global running
     global mouse_x, mouse_y
-    global print_x, print_y
+    global hand_positions
 
     events = get_events()
     for event in events:
@@ -20,23 +20,21 @@ def handle_events():
             mouse_x, mouse_y = event.x, TUK_HEIGHT - 1 - event.y
         elif event.type == SDL_MOUSEBUTTONDOWN:
             if event.button == SDL_BUTTON_LEFT:
-                print_x, print_y = mouse_x, mouse_y
+                hand_positions.append((mouse_x,mouse_y))
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
 
-def draw_hand(x, y):
-    hand.draw(x, y)
-
 running = True
 mouse_x, mouse_y = TUK_WIDTH // 2, TUK_HEIGHT // 2
-print_x, print_y = 0, 0
+hand_positions = []
 hide_cursor()
 
 while running:
     clear_canvas()
     TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
     hand.draw(mouse_x, mouse_y)
-    draw_hand(print_x, print_y)
+    for x,y in hand_positions:
+        hand.draw(x,y)
     update_canvas()
     handle_events()
 
