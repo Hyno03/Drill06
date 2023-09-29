134d4bb5a6d40f3719834fe5bdd06e237402bd23
Hyno03 <jenny2117@naver.com>
2023-09-29 20:07:05 +0900

    Draw / Quit event
    draw background
    draw cursor into handprint image
    code quit event

---
+++ character_trace_hand_by_mouse.py
@@ -0,0 +1,33 @@
+from pico2d import *
+
+TUK_WIDTH, TUK_HEIGHT = 1280, 1024
+open_canvas(TUK_WIDTH, TUK_HEIGHT)
+
+TUK_ground = load_image('TUK_GROUND.png')
+character = load_image('animation_sheet.png')
+hand = load_image('hand_arrow.png')
+
+def handle_events():
+    global running
+    global mouse_x, mouse_y
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_MOUSEMOTION:
+            mouse_x, mouse_y = event.x, TUK_HEIGHT - 1 - event.y
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+
+running = True
+mouse_x, mouse_y = TUK_WIDTH // 2, TUK_HEIGHT // 2
+hide_cursor()
+
+while running:
+    clear_canvas()
+    TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
+    hand.draw(mouse_x, mouse_y)
+    update_canvas()
+    handle_events()
+
+close_canvas()
